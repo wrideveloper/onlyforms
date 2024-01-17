060a87f784101c2def70fa72bb2b94d4612528e0
elianiva <dicha.arkana03@gmail.com>
2024-01-17 08:25:23 +0700

    chore: ci failing is corrected

--- src/lib/server/repositories/db-client.ts
+++ src/lib/server/repositories/db-client.ts
@@ -2,7 +2,7 @@ import { MongoClient } from "mongodb";
 import { MONGO_DB, MONGO_HOST, MONGO_PASSWORD, MONGO_PORT, MONGO_USERNAME } from "$env/static/private";
 
 let connectionUri = "";
-if (MONGO_HOST === "localhost") {
+if (MONGO_HOST === "localhost" || MONGO_HOST === "mongodb" || MONGO_HOST === "db") {
 	connectionUri = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}?retryWrites=true&w=majority&authSource=admin`;
 } else {
 	connectionUri = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}?retryWrites=true&w=majority&authSource=admin`;
